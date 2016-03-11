d0b3b23e601d56d845b16e6a75a5ed1b28bda67b
Sam Nnodim <sam.nnodim@gmail.com>
2016-03-11 23:34:09 +0000

    Added new apikey

--- check-weather.py
+++ check-weather.py
@@ -5,7 +5,7 @@
 #
 # adapted from: https://github.com/mikedewar/RealTimeStorytelling
 
-# import some v useful libs
+# import some very useful libs
 from sys import stdout
 import requests
 import json
@@ -15,13 +15,15 @@ import random as r
 # this script polls the soundcloud API, looking for new songs uploaded to
 # the platform.
 
-listofSongs = {}
-url = "http://api.openweathermap.org/data/2.5/weather?zip=10025,us&appid="
-apikey = "2df8905bba963ddd6ffe981af70669fb" # OpenWeatherMap API key
+url = "http://api.openweathermap.org/data/2.5/weather?id=4900961&appid="
+apikey = "b1b15e88fa797225412429c1c50c122a" # OpenWeatherMap API key
 
 while True:
     # send a GET request to the OWM API and turn it to JSON data
     data = requests.get( url + apikey ).json()
+
+    # get the humidity from the data
+    humidity = data["main"]["humidity"]
 
     # Get weather params from data
     print ( json.dumps(data) )
