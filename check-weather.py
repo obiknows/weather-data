195647ae367ceb7e74c906038f451779b3c6ac84
Sam Nnodim <sam.nnodim@gmail.com>
2016-03-12 00:02:26 +0000

    Added poisson params

--- check-weather.py
+++ check-weather.py
@@ -7,10 +7,10 @@
 
 # import some very useful libs
 from sys import stdout
+import numpy as np
 import requests
 import json
 import time
-import random as r
 
 # this script polls the soundcloud API, looking for new songs uploaded to
 # the platform.
@@ -18,15 +18,20 @@ import random as r
 url = "http://api.openweathermap.org/data/2.5/weather?id=4900961&appid="
 apikey = "b1b15e88fa797225412429c1c50c122a" # OpenWeatherMap API key
 
+# Set poission param. (2 minutes)
+rate = 60
+
 while True:
     # send a GET request to the OWM API and turn it to JSON data
     data = requests.get( url + apikey ).json()
 
-    # get the humidity from the data
-    humidity = data["main"]["humidity"]
+    # get the pressure from the data
+    pressure = data["main"]["pressure"]
 
-    # Get weather params from data
-    print ( json.dumps(data) )
+    # Print current weather pressure from params
+    print ( json.dumps({"pressure": pressure}) )
+    stdout.flush()
 
-    # sleep for 2 mins and poll again
-    time.sleep( 120 )
+    # set the sleep rate as a randomn exponential value
+    # draw time beteween events from a random distribution
+    time.sleep( np.random.exponential(rate) )
